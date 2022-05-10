4d1d7736c587c14fe78f8387597bbc8e5a5fae12
cheronika <95296661+cheronika@users.noreply.github.com>
2022-05-10 16:51:17 +0300

    Update for_founders.py

--- for_founders.py
+++ for_founders.py
@@ -4,7 +4,7 @@ from work_with_db import add_new_found_animal
 from telegram.ext import ConversationHandler
 
 
-def i_found(update, context):  # функция начала диалога с человеком, который наашел животное
+def i_found(update, context):
     update.message.reply_text("Так, опишите найденного питомца по пунктам "
                               "Если на какой-то вопрос Вы не можете или не хотите ответить, поставьте '-'. "
                               "Вы можете прервать Ваше обращение, написав боту '/stop', но тогда оно не будет записано")
@@ -18,28 +18,52 @@ def found_date(update, context):
     return 2
 
 
-def description_of_found_animal(update, context):
+def f_what_animal(update, context):
     day = int(str(update.message.text).split('-')[0])
     month = int(str(update.message.text).split('-')[1])
     year = int('20' + str(update.message.text).split('-')[2])
     context.user_data['date'] = datetime.date(year=year, month=month, day=day)
-    update.message.reply_text("Опишите в нескольких предложениях найденное животное")
-    print(context.user_data)
+    update.message.reply_text("Какое животное Вы нашли? Напишите его вид, например, собака, кошка или хомяк")
     return 3
 
 
-def found_place(update, context):
-    context.user_data['description'] = update.message.text
-    update.message.reply_text("Напишите адрес, где Вы нашли это животное, как можно точнее.")
-    print(context.user_data)
+def f_have_stamp(update, context):
+    context.user_data['animal'] = update.message.text
+    update.message.reply_text("Есть ли у этого животного клеймо? Ответьте 'да' или 'нет'")
     return 4
 
 
+def f_have_collar(update, context):
+    context.user_data['stamp'] = update.message.text.lower()
+    if context.user_data['stamp'] == 'да':
+        context.user_data['stamp'] = True
+    else:
+        context.user_data['stamp'] = False
+    update.message.reply_text("Есть ли у этого животного ошейник? Ответьте 'да' или 'нет'")
+    return 5
+
+
+def found_place(update, context):
+    context.user_data['collar'] = update.message.text.lower()
+    if context.user_data['collar'] == 'да':
+        context.user_data['collar'] = True
+    else:
+        context.user_data['collar'] = False
+    update.message.reply_text("Напишите адрес, где Вы нашли это животное, как можно точнее.")
+    return 6
+
+
+def description_of_found_animal(update, context):
+    context.user_data['found_place'] = update.message.text
+    update.message.reply_text("Опишите в нескольких предложениях найденное животное")
+    return 7
+
+
 def founder_phone_number(update, context):
-    context.user_data['place'] = update.message.text
+    context.user_data['description'] = update.message.text
     update.message.reply_text("Пожалуйста, отправьте боту свой номер телефона, "
                               "чтобы хозяин животного мог с Вами связаться.")
-    return 5
+    return 9
 
 
 def goodby_founder(update, context):
