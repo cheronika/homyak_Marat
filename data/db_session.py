7c054cf39d75f25e931e1535d625a0050e7aba9b
cheronika <95296661+cheronika@users.noreply.github.com>
2022-05-10 16:53:06 +0300

    Update db_session.py

--- data/db_session.py
+++ data/db_session.py
@@ -2,8 +2,6 @@ import sqlalchemy as sa
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
 import sqlalchemy.ext.declarative as dec
-from . import __all_models
-
 
 SqlAlchemyBase = dec.declarative_base()
 
