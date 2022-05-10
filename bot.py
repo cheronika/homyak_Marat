9082110a6944b916ad4b95836c51cb7c0303b7d0
cheronika <95296661+cheronika@users.noreply.github.com>
2022-05-10 16:47:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,13 +2,12 @@ import logging
 from telegram.ext import Updater, MessageHandler, Filters, ConversationHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import CommandHandler
-from for_owners import i_lost, lost_date, description_of_lost_animal, lost_place, owner_phone_number, goodby_owner
+from for_owners import i_lost, lost_date, description_of_lost_animal, lost_place, owner_phone_number, goodby_owner, \
+    search_in_db, radius, l_have_stamp, l_have_collar, l_what_animal
 from for_founders import i_found, found_date, description_of_found_animal, found_place, founder_phone_number, \
-    goodby_founder
-from work_with_db import db_session
-from data.__all_models import Lost, Found
+    goodby_founder, f_have_stamp, f_have_collar, f_what_animal
 from work_with_map import lost_map, found_map
-
+from for_stop_searching import stop_searching, cancel_searching
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
@@ -18,10 +17,8 @@ logger = logging.getLogger(__name__)
 
 TOKEN = "5348701239:AAF5RFIr4Fng7UCcbYQRT_xLcg_O5aVflhc"
 
-information_about_user = {}  # словарь, в который будет записываться вся информация о животном
+information_about_user = {} 
 
-
-# базовые команды бота
 
 def start(update, context):
     update.message.reply_text("Привет! Я бот для поиска пропавших животных.", reply_markup=markup)
@@ -32,9 +29,9 @@ def help(update, context):
         "Тут будет написано, как со мной работать")
 
 
-# кнопки, которые появляются при начале диалога с ботом
 reply_keyboard = [['/i_lost'],
                   ['/i_found'],
+                  ['/stop_searching'],
                   ['/map_1'],
                   ['/map_2']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
@@ -72,11 +69,16 @@ def main():
         entry_points=[CommandHandler('i_lost', i_lost)],
         states={
             1: [MessageHandler(Filters.text & ~Filters.command, lost_date, pass_user_data=True)],
-            2: [MessageHandler(Filters.text & ~Filters.command, description_of_lost_animal, pass_user_data=True)],
-            3: [MessageHandler(Filters.text & ~Filters.command, lost_place, pass_user_data=True)],
-            4: [MessageHandler(Filters.text & ~Filters.command, owner_phone_number, pass_user_data=True)],
-            5: [MessageHandler(Filters.text & ~Filters.command, goodby_owner, pass_user_data=True)]
-            # добавить обработку фото
+            2: [MessageHandler(Filters.text & ~Filters.command, l_what_animal, pass_user_data=True)],
+            3: [MessageHandler(Filters.text & ~Filters.command, l_have_stamp, pass_user_data=True)],
+            4: [MessageHandler(Filters.text & ~Filters.command, l_have_collar, pass_user_data=True)],
+            5: [MessageHandler(Filters.text & ~Filters.command, lost_place, pass_user_data=True)],
+            6: [MessageHandler(Filters.text & ~Filters.command, radius, pass_user_data=True)],
+            7: [MessageHandler(Filters.text & ~Filters.command, search_in_db, pass_user_data=True)],
+            8: [MessageHandler(Filters.text & ~Filters.command, description_of_lost_animal, pass_user_data=True)],
+            9: [MessageHandler(Filters.text & ~Filters.command, owner_phone_number, pass_user_data=True)],
+            # 10: [MessageHandler(Filters.text & ~Filters.command, add_photo, pass_user_data=True)],
+            11: [MessageHandler(Filters.text & ~Filters.command, goodby_owner, pass_user_data=True)]
         },
         fallbacks=[CommandHandler('stop', stop)]
     )
@@ -85,15 +87,26 @@ def main():
         entry_points=[CommandHandler('i_found', i_found)],
         states={
             1: [MessageHandler(Filters.text & ~Filters.command, found_date, pass_user_data=True)],
-            2: [MessageHandler(Filters.text & ~Filters.command, description_of_found_animal, pass_user_data=True)],
-            3: [MessageHandler(Filters.text & ~Filters.command, found_place, pass_user_data=True)],
-            4: [MessageHandler(Filters.text & ~Filters.command, founder_phone_number, pass_user_data=True)],
-            5: [MessageHandler(Filters.text & ~Filters.command, goodby_founder, pass_user_data=True)]
-            # добавить обработку фото
+            2: [MessageHandler(Filters.text & ~Filters.command, f_what_animal, pass_user_data=True)],
+            3: [MessageHandler(Filters.text & ~Filters.command, f_have_stamp, pass_user_data=True)],
+            4: [MessageHandler(Filters.text & ~Filters.command, f_have_collar, pass_user_data=True)],
+            5: [MessageHandler(Filters.text & ~Filters.command, found_place, pass_user_data=True)],
+            6: [MessageHandler(Filters.text & ~Filters.command, description_of_found_animal, pass_user_data=True)],
+            7: [MessageHandler(Filters.text & ~Filters.command, founder_phone_number, pass_user_data=True)],
+            # 8: [MessageHandler(Filters.text & ~Filters.command, f_add_photo, pass_user_data=True)],
+            9: [MessageHandler(Filters.text & ~Filters.command, goodby_founder, pass_user_data=True)]
         },
         fallbacks=[CommandHandler('stop', stop)]
     )
     dp.add_handler(conv_handler_2)
+    conv_handler_3 = ConversationHandler(
+        entry_points=[CommandHandler('stop_searching', stop_searching)],
+        states={
+            1: [MessageHandler(Filters.text & ~Filters.command, cancel_searching)]
+        },
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+    dp.add_handler(conv_handler_3)
     updater.start_polling()
     updater.idle()
 
